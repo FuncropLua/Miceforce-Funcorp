261e9e27932d047d31bf97cd3c026227d0f084d3
Kuhaku <realmeltedfruit@gmail.com>
2019-09-30 18:30:22 -0400

    Fly Script.lua
    Press s, down arrow or space to fly.

---
+++ Fly Script.lua
@@ -0,0 +1,15 @@
+function eventNewPlayer(name)
+	for _, keys in next, { 32, 3 } do
+		system.bindKeyboard(name, key, true)
+	end
+end
+
+for name in next, tfm.get.room.playerList do
+	eventNewPlayer(name)
+end
+
+function eventKeyboard(name, key, down, x, y)
+	if tfm.get.room.playerList[name].isDead == false then
+		tfm.exec.movePlayer(name, 0, 0, false, 0, -80, false)
+	end
+end
