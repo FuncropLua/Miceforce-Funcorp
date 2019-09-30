7caecc088defe7149e4ea835174a0c4be2f8b5cb
Kuhaku <realmeltedfruit@gmail.com>
2019-09-30 19:10:11 -0400

    Fly script
    Added mort command.

--- Fly Script.lua
+++ Fly Script.lua
@@ -8,6 +8,12 @@ for name in next, tfm.get.room.playerList do
 	eventNewPlayer(name)
 end
 
+function eventChatCommand(name, message)
+	if message == "die" or message == "mort" or message == "kill" then
+		tfm.exec.killPlayer(name)
+	end
+end
+
 function eventKeyboard(name, key, down, x, y)
 	if tfm.get.room.playerList[name].isDead == false then
 		tfm.exec.movePlayer(name, 0, 0, false, 0, -80, false)
