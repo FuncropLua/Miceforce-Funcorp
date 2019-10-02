d5f1b2de29b1f1efbb76945ee4e534854a1d5544
Kuhaku <14054031+FuncropLua@users.noreply.github.com>
2019-10-01 20:55:56 -0400

    Update Spawn objects with z and x.lua

--- Spawn objects with z and x.lua
+++ Spawn objects with z and x.lua
@@ -12,15 +12,23 @@ local text = [[
 <a href = 'event:39'><font color = '#2F7FCC'>•<J> Apple</a>
 <a href = 'event:6'><font color = '#2F7FCC'>•<J> Ball</a>
 <a href = 'event:23'><font color = '#2F7FCC'>•<J> Bomb</a>
-<a href = 'event:27'><font color = '#2F7FCC'>•<J> Cannon</a>
+<a href = 'event:17'><font color = '#2F7FCC'>•<J> Cannon</a>
 <a href = 'event:33'><font color = '#2F7FCC'>•<J> Chicken</a>
 <a href = 'event:34'><font color = '#2F7FCC'>•<J> Snowball</a>
 <a href = 'event:95'><font color = '#2F7FCC'>•<J> Paper Ball</a>
 <a href = 'event:89'><font color = '#2F7FCC'>•<J> Pumpkin Ball</a>
 ]]
-local items = { 10 = true, 39 = true, 6 = true, 23 = true, 27 = true, 33 = true , 34 = true, 95 = true, 89 = true }
-local funcrops = { ["Daichi"] = true, ["Icey00008"] = true, ["Kalani"] = true, ["Karl"] = true, ["+zen"] = true }
+local items = { [10] = true, [39] = true, [6] = true, [23] = true, [17] = true, [33] = true , [34] = true, [95] = true, [89] = true }
 local obj, speed, count = 17, 25, 0
+
+function check()
+	local a = { 1, 2, 3, 4 }
+	return #a[1]
+end
+
+local _, error = pcall(check)
+
+local loader = string.match(error, "[^:]+")
 
 function eventNewPlayer(name)
 	for _, keys in next, { 17, 88, 90 } do
@@ -43,7 +51,7 @@ function eventKeyboard(name, key, down, x, y)
 			end
 			count = count + 1
 		end
-	elseif key == 17 and funcrops[name] then
+	elseif key == 17 and name == loader then
 		if down then
 			ui.addTextArea(1, text, name, 10, 30, 120, nil, 1, 1, 1)
 		else
@@ -57,6 +65,7 @@ function eventKeyboard(name, key, down, x, y)
 				tfm.exec.removeObject(o)
 			end
 		end
+		count = 0
 	end
 end
 
@@ -77,7 +86,7 @@ function eventChatCommand(name, message)
 	
 	if c == "mort" or c == "die" or c == "kill" then
 		tfm.exec.killPlayer(name)
-	elseif c == "speed" and funcrops[name] then
+	elseif c == "speed" and name == loader then
 		if args[1] and type(tonumber(args[1])) == "number" then
 			local ns = tonumber(args[1])
 			if ns >= 5 and ns <= 25 then
@@ -88,11 +97,12 @@ function eventChatCommand(name, message)
 		else
 			speed = 25
 		end
-	elseif c == "clear" or c == "c" and funcrops[name] then
+	elseif c == "clear" or c == "c" and name == loader then
 		for o, p in next, tfm.get.room.objectList do
 			if items[p.type] then
 				tfm.exec.removeObject(o)
 			end
 		end
+		count = 0
 	end
 end
