776dde2525c0da5993826219abd4c09b9d049feb
Kuhaku <14054031+FuncropLua@users.noreply.github.com>
2019-10-01 20:00:43 -0400

    Update Spawn cannons with z and x.lua

--- Spawn cannons with z and x.lua
+++ Spawn cannons with z and x.lua
@@ -1,16 +1,30 @@
 --[[
-Since /mort will not work, mice can type !die, !mort or !kill to mort instead.
-You can change the cannon speed by using !speed command, the range should be from 20-100.
-e.g: !speed 90
+• Since /mort will not work, mice can type !die, !mort or !kill to mort instead.
+• You can change the object speed by using !speed command, the range should be from 5-25.
+	e.g: !speed 10
+• Objects will despawn (not all) to prevent lag/crash.
+• Hold ctrl to select object
 ]]
 
+local text = [[
+<R><font size = '14'>Choose One:
+<a href = 'event:10'><font color = '#2F7FCC'>•<J> Anvil</a>
+<a href = 'event:39'><font color = '#2F7FCC'>•<J> Apple</a>
+<a href = 'event:6'><font color = '#2F7FCC'>•<J> Ball</a>
+<a href = 'event:23'><font color = '#2F7FCC'>•<J> Bomb</a>
+<a href = 'event:27'><font color = '#2F7FCC'>•<J> Cannon</a>
+<a href = 'event:33'><font color = '#2F7FCC'>•<J> Chicken</a>
+<a href = 'event:34'><font color = '#2F7FCC'>•<J> Snowball</a>
+<a href = 'event:95'><font color = '#2F7FCC'>•<J> Paper Ball</a>
+<a href = 'event:89'><font color = '#2F7FCC'>•<J> Pumpkin Ball</a>
+]]
 local funcrops = { ["Daichi"] = true, ["Icey00008"] = true, ["Kalani"] = true, ["Karl"] = true, ["+zen"] = true }
-local speed = 70
+local obj, speed, count = 17, 30, 0
 
-keys = { z = 90, x = 88 }
 function eventNewPlayer(name)
-	for _, keys in next, keys do
+	for _, keys in next, { 17, 88, 90 } do
 		system.bindKeyboard(name, keys, true)
+		system.bindKeyboard(name, keys, false)
 	end
 end
 
@@ -18,20 +32,55 @@ for name in next, tfm.get.room.playerList do
 	eventNewPlayer(name)
 end
 
-function eventKeyboard(name, keys, down, x, y)
-	if tfm.get.room.playerList[name].isDead == false then
-		tfm.exec.addShamanObject(17, x + (keys == keys.x and 35 or keys == keys.z and -35), y, keys == keys.x and 90 or keys == keys.z and -90, speed)
+function eventKeyboard(name, key, down, x, y)
+	if key == 88 or key == 90 and tfm.get.room.playerList[name].isDead == false then
+		if down then
+			if obj == 17 then
+				tfm.exec.addShamanObject(obj, x + (key == 88 and 35 or key == 90 and -35), y - 20, key == 88 and 90 or key == 90 and -90)
+			else
+				tfm.exec.addShamanObject(obj, x + (key == 88 and 35 or key == 90 and -35), y - 20, 0, key == 88 and speed or key == 90 and -speed)
+			end
+			count = count + 1
+		end
+	elseif key == 17 and funcrops[name] then
+		if down then
+			ui.addTextArea(1, text, name, 10, 30, 120, nil, 1, 1, 1)
+		else
+			ui.removeTextArea(1, name)
+		end
+	end
+	
+	if count == 10 then
+		for o, p in next, tfm.get.room.objectList do
+			if p.type == 17 then
+				tfm.exec.removeObject(o)
+			end
+		end
 	end
 end
 
+function eventTextAreaCallback(id, name, cb)
+		obj = cb
+end
+
 function eventChatCommand(name, message)
-	if message == "mort" or message == "die" or message == "kill" then
+	local args, p, c = { }, 0, ""
+	for slice in string.gmatch(message, "%S+") do
+			if p == 0 then
+				c = string.lower(slice)
+			else 
+				args[p] = slice
+			end
+			p = p + 1
+	end
+	
+	if c == "mort" or c == "die" or c == "kill" then
 		tfm.exec.killPlayer(name)
-	elseif funcrops[name] and string.sub(message, 1, 5) == "speed" then
-		local new_speed = tonumber(string.sub(message, 7))
-		if type(new_speed) == "number" then
-			if new_speed >= 20 and new_speed =< 100 then
-				speed = new_speed
+	elseif c == "speed" and funcrops[name] then
+		if args[1] and type(tonumber(args[1])) == "number" then
+			local ns = tonumber(args[1])
+			if ns >= 5 and ns <= 25 then
+				speed = ns
 			else
 				speed = 70
 			end
